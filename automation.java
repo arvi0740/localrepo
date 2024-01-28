b484ede80f2bce6eeba4fc09f6b5ed4cee0c776f
Arvind Kumar <arvindverma475@gmail.com>
2024-01-29 04:57:10 +0530

    update the creds

--- automation.java
+++ automation.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
 
 public class automation
 {
@@ -20,24 +19,24 @@ public class automation
         driver.get("https://www.facebook.com/");
         Thread.sleep(400);
         WebElement userName= driver.findElement(By.id("email"));
-        userName.sendKeys("9415698321");
+        userName.sendKeys("enter the ID");
         Thread.sleep(200);
         WebElement passWord = driver.findElement(By.id("pass"));
-        passWord.sendKeys("pragyarvind");
+        passWord.sendKeys("enter the Pass");
         Thread.sleep(200);
         WebElement login = driver.findElement(By.name("login"));
         login.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        Thread.sleep(1000);
-
-        WebElement profile= driver.findElement(By.xpath("//*[name()='g' and contains(@mask,'url(#:R6km')]//*[name()='image' and contains(@x,'0')]"));
-        profile.click();
-        Thread.sleep(2000);
-        WebElement logout = driver.findElement(By.xpath("//span[normalize-space()='Log Out']"));
-
-
-        logout.click();
-        Thread.sleep(400);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        Thread.sleep(1000);
+//
+//        WebElement profile= driver.findElement(By.xpath("//*[name()='g' and contains(@mask,'url(#:R6km')]//*[name()='image' and contains(@x,'0')]"));
+//        profile.click();
+//        Thread.sleep(2000);
+//        WebElement logout = driver.findElement(By.xpath("//span[normalize-space()='Log Out']"));
+//
+//
+//        logout.click();
+//        Thread.sleep(400);
 
         driver.quit();
 
